9a18183cf072541dc4641bb8b748c38b7d4efb57
kamil <kmlrydzewski@gmail.com>
2020-05-23 11:35:05 +0200

    used different api

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const endpoint = "http://data.fixer.io/api/latest?access_key=63bf9685968808c6ecdc616d73c6e2f6";
+const endpoint = "https://api.exchangeratesapi.io/latest";
 
 const form = document.querySelector("form");
 const inputDropdowns = document.querySelectorAll(".selected");
